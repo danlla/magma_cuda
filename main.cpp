e497eb2369b9d823c1384fcc3ac6bc0faf069590
danila <danila_glazkov@mail.ru>
2021-12-20 09:37:53 +0400

    fix

--- main.cpp
+++ main.cpp
@@ -4,14 +4,15 @@
 #include "magma.cuh"
 #include "magma_gpu.cuh"
 #include <fstream>
+#include "argparse.hpp"
 
 void bench(const magma& m, size_t n, const std::unique_ptr<magma::block[]>& message);
 
-void encrypt_file(const magma& m, const char* input_file, const char* output_file);
+void encrypt_file(const magma& m, const char* input_file, const char* output_file, size_t buf_size);
 
-void decrypt_file(const magma& m, const char* input_file, const char* output_file);
+void decrypt_file(const magma& m, const char* input_file, const char* output_file, size_t buf_size);
 
-int main()
+int main(int argc, char* argv[])
 {
     std::array<unsigned int, 8> keys = { 0xccddeeff, 0x8899aabb, 0x44556677, 0x00112233, 0xf3f2f1f0, 0xf7f6f5f4, 0xfbfaf9f8, 0xfffefdfc };
     magma m(keys);
@@ -21,7 +22,7 @@ int main()
 
     std::mt19937 engine;
     engine.seed(1);
-    size_t n = 128*1024 / sizeof(magma::block);//64*1024*1024;
+    size_t n = 128 * 1024 / sizeof(magma::block);//64*1024*1024;
     auto message = std::make_unique<magma::block[]>(n);
     for (size_t i = 0; i < n; ++i)
     {
@@ -30,11 +31,78 @@ int main()
     }
     //bench(m, n, message);
 
-    //magma_gpu m2(keys);
+    magma_gpu m2(keys);
     //bench(m2, n, message);
 
-    encrypt_file(m, "test.txt","encrypted.txt");
-    decrypt_file(m, "encrypted.txt", "decrypted.txt");
+    //encrypt_file(m, "test.txt","encrypted.txt",500);
+    //decrypt_file(m, "encrypted.txt", "decrypted.txt",500);
+
+    argparse::ArgumentParser program("magma", "1.0.0");
+    program.add_argument("-e", "--encrypt")
+        .help("encrypt data")
+        .default_value(false)
+        .implicit_value(true);
+    program.add_argument("-d", "--decrypt")
+        .help("decrypt data")
+        .default_value(false)
+        .implicit_value(true);
+    program.add_argument("-b", "--bench")
+        .help("bench")
+        .default_value(false)
+        .implicit_value(true);
+    program.add_argument("-r", "--random")
+        .help("random key")
+        .default_value(false)
+        .implicit_value(true);
+    program.add_argument("-p", "--password")
+        .help("password key");
+    program.add_argument("key file")
+        .help("for encrypt with random/password write new key in file\n\t\tfor encrypt without random/password read a key from file\n\t\tfor decrypt read a key from the file");
+    program.add_argument("input file")
+        .help("input file");
+    program.add_argument("output file")
+        .help("output file");
+    try {
+        program.parse_args(argc, argv);
+    }
+    catch (const std::runtime_error& err) {
+        std::cerr << err.what() << std::endl;
+        std::cerr << program;
+        std::exit(1);
+    }
+    if (program["--encrypt"] == true) {
+        if (program["--decrypt"] == true)
+        {
+            std::cout << "Only encrypt or decrypt";
+            exit(EXIT_FAILURE);
+        }
+        std::cout << "encrypt" << std::endl;
+    }
+    if (program["--decrypt"] == true) {
+        std::cout << "decrypt" << std::endl;
+    }
+    if (program["--bench"] == true) {
+        std::cout << "bench" << std::endl;
+    }
+    if (program["--random"] == true) {
+        if (program.present("-p").has_value())
+        {
+            std::cout << "Only password or random";
+            exit(EXIT_FAILURE);
+        }
+        std::cout << "random" << std::endl;
+    }
+    /* auto pass = program.present("-p");
+     std::cout << pass.value() << std::endl;*/
+    auto key = program.get<std::string>("key file");;
+    std::cout << key << std::endl;
+    auto inputf = program.get<std::string>("input file");
+    std::cout << inputf << std::endl;
+    auto outputf = program.get<std::string>("output file");
+    std::cout << outputf << std::endl;
+
+    //  ./magma -e -k key input output
+
 }
 
 int addition(magma::block* buf, size_t byte)
@@ -47,61 +115,61 @@ int addition(magma::block* buf, size_t byte)
     }
     else
     {
-        int dif = byte % 8;
-        for (int i = dif; i < 8; ++i)
+        int dif = 8 - byte % 8;
+        for (int i = 8 - dif; i < 8; ++i)
             buf[byte / 8].c[i] = dif;
     }
     return 1;
 }
 
-int cut_addition(magma::block* buf, size_t n)
+int cut_addition(const magma::block& b)
 {
-    if (buf[n-1].c[7] == 8)
-        return 8;
-    int dif = 8-buf[n-1].c[7];
-    /*for (int i = 7; i >= dif; --i)
-        buf[n-1].c[i] = 0;*/
-    return dif;
+    return b.c[7];
 }
 
-void encrypt_file(const magma& m, const char* input_file, const char* output_file)
+void encrypt_file(const magma& m, const char* input_file, const char* output_file, size_t buf_size)
 {
-    std::ifstream ifile(input_file);
-    std::ofstream ofile(output_file);
-    magma::block* buf = new magma::block[501];
+    std::ifstream ifile(input_file, std::ios::binary);
+    std::ofstream ofile(output_file, std::ios::binary);
+    magma::block* buf = new magma::block[buf_size + 1];
+    size_t count = 0;
     while (true)
     {
-        ifile.read((char*)buf, 500 * sizeof(magma::block));
-        auto count = ifile.gcount();
-        if (!count)
+        ifile.read((char*)buf, buf_size * sizeof(magma::block));
+        count = ifile.gcount();
+        std::cout << "read " << count << std::endl;
+        if (!ifile)
             break;
-        std::cout <<"read " << ifile.gcount() << std::endl;
-        int add = 0;
-        if(count!=4000)
-            add = addition(buf, count);
-        m.encrypt(buf, count / 8 + add);
-        ofile.write((char*)buf, (count / 8 + add) * sizeof(magma::block));
+        m.encrypt(buf, count / 8);
+        ofile.write((char*)buf, (count / 8) * sizeof(magma::block));
     }
+    int add = addition(buf, count);
+    m.encrypt(buf, count / 8 + add);
+    ofile.write((char*)buf, (count / 8 + add) * sizeof(magma::block));
+    delete[] buf;
 }
 
-void decrypt_file(const magma& m, const char* input_file, const char* output_file)
+void decrypt_file(const magma& m, const char* input_file, const char* output_file, size_t buf_size)
 {
-    std::ifstream ifile(input_file);
-    std::ofstream ofile(output_file);
-    magma::block* buf = new magma::block[501];
-    while(true)
+    std::ifstream ifile(input_file, std::ios::binary);
+    std::ofstream ofile(output_file, std::ios::binary);
+    magma::block* buf = new magma::block[buf_size + 1];
+    size_t count = 0;
+    while (true)
     {
-        ifile.read((char*)buf, 500 * sizeof(magma::block));
-        auto count = ifile.gcount();
-        if (!count)
+        ifile.read((char*)buf, buf_size * sizeof(magma::block));
+        count = ifile.gcount();
+        std::cout << "read " << count << std::endl;
+        if (!ifile)
             break;
-        std::cout <<"read " << count << std::endl;
         m.decrypt(buf, count / 8);
-        int extra = 0;
-        if(count!=4000)
-            extra = cut_addition(buf, count / 8);
-        ofile.write((char*)buf, (count / 8) * sizeof(magma::block) - extra);
-    } 
+        ofile.write((char*)buf, (count / 8) * sizeof(magma::block));
+    }
+    int extra = 0;
+    m.decrypt(buf, count / 8);
+    extra = cut_addition(buf[count / 8 - 1]);
+    ofile.write((char*)buf, (count / 8) * sizeof(magma::block) - extra);
+    delete[] buf;
 }
 
 /*one task - one function*/
